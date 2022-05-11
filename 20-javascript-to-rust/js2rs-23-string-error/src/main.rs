b6ff858264f275090b6078ce2bb85663cdbd0a5c
katopz <katopz@gmail.com>
2022-05-12 00:35:15 +0700

    refactor

--- 20-javascript-to-rust/js2rs-23-string-error/src/main.rs
+++ 20-javascript-to-rust/js2rs-23-string-error/src/main.rs
@@ -1,18 +1,39 @@
-// Main function
-fn main() {
-    // Call greet function
-    greet("World" /*.to_string()*/); // 💡 Expected error, What is the problem?
+fn example_to_owned() {
+    // # to_owned()
+    // Own a string from &str by cloning it
+    let foo = "foo".to_owned();
+    println!("foo = {}", foo);
+}
+
+fn example_to_string() {
+    // # to_string()
+    // Call greet function that requires a String
+    greet("World" /*.to_string()*/);
+    // ❌ expected struct `String`, found `&str`
+}
+
+fn example_into() {
+    // # into()
+    // Convert &str to String
+    let foo/*: String*/ = "foo".into();
+    // ❌ ^^^ consider giving `foo` a type
+}
+
+fn example_format() {
+    // format!()
+    let bar = "bar";
+    let foobar = format!("foo {bar}"); // 🐥 `foo ${bar}`
+    println!("foobar = {foobar}");
 }
 
 // Greet function
 fn greet(target: String) {
-    // console.log(`Hello, ${target}`);
-    println!("Hello, {}", target);
+    println!("Hello, {}", target); // 🐥 console.log(`Hello, ${target}`);
 }
 
-// Test function
-#[test]
-fn test() {
-    // Call main function
-    main();
+fn main() {
+    example_to_owned();
+    example_to_string();
+    example_into();
+    example_format();
 }
--- 20-javascript-to-rust/js2rs-24-borrow/src/main.rs
+++ 20-javascript-to-rust/js2rs-24-borrow/src/main.rs
@@ -1,9 +1,24 @@
-fn main() {
+fn shadow() {
     // foo is integer
+    // var foo = 1;
     let foo = 1;
     println!("{}", foo);
 
     // foo is string
-    let foo = "3";
+    // var foo = "foo";
+    let foo = "foo";
     println!("{}", foo);
+
+    // foo is string
+    let /*mut*/ bar= "bar";
+    // bar = "lol";
+    // ❌ ^^^^^^^^^^^ cannot assign twice to immutable variable
+    println!("{}", bar);
+}
+
+fn borrow() {}
+
+fn main() {
+    shadow();
+    borrow();
 }
