230b4b8acfa43df49b9681b9d3dddbf44c556da1
katopz <katopz@gmail.com>
2022-05-24 18:02:50 +0700

    add mut

--- 20-javascript-to-rust/204-borrow/src/main.rs
+++ 20-javascript-to-rust/204-borrow/src/main.rs
@@ -19,12 +19,60 @@ fn mutable() {
 }
 
 
-fn borrow() {
+fn string_convert() {
+    let foo_str = "foo";
+    let mut foo_string: String = foo_str.to_string();
+    println!("foo_str:{}", foo_str);
+    println!("foo_string:{}", foo_string);
 
+    foo_string = "baz".to_string();
+    println!("foo_string:{}", foo_string);
+
+    foo_string = "baz baz".to_owned();
+    println!("foo_string:{}", foo_string);
+
+    foo_string = "baz baz baz".into();
+    println!("foo_string:{}", foo_string);
+}
+
+fn borrow() {
+    // immutable
+    bar_str("foo");
+    bar_string("foo".to_string());
+
+    // mutable
+    let mut foo_str = "foo";
+    bar_str_mut(&mut foo_str);
+    println!("3️⃣ target_str:{}", foo_str);
+
+    let mut foo_string = "foo".to_owned();
+    bar_string_mut( &mut foo_string);
+    println!("3️⃣ target_string:{}", foo_string);
+}
+
+fn bar_str(target_str: &str) {
+    println!("1️⃣ target_str:{}", target_str);
+}
+
+fn bar_string(target_string: String) {
+    println!("1️⃣ target_string:{}", target_string);
+}
+
+fn bar_str_mut(target_str: &mut &'static str) {
+    println!("1️⃣ target_str:{}", target_str);
+    *target_str = "baz";
+    println!("2️⃣ target_str:{}", target_str);
+}
+
+fn bar_string_mut(target_string: &mut String) {
+    println!("1️⃣ target_string:{}", target_string);
+    *target_string = "baz".to_owned();
+    println!("2️⃣ target_string:{}", target_string);
 }
 
 fn main() {
     shadow();
     mutable();
+    string_convert();
     borrow();
 }
